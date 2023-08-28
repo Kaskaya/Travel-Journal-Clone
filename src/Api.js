f75497b5be9857f99f615c59e6f0d7f9e0db9575
Kaskaya <recep.tkaskaya@gmail.com>
2023-08-28 04:24:08 +0300

    Created an api object

---
+++ src/Api.js
@@ -0,0 +1,28 @@
+const Api = [
+  {
+    id: 1,
+    img: "https://rb.gy/hxfl8",
+    city: "Amsterdam",
+    country: "NETHERLANDS",
+    date: "12 Jan, 2021 - 24 Jan, 2021",
+    text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+  },
+  {
+    id: 1,
+    img: "https://rb.gy/wld93",
+    city: "London",
+    country: "ENGLAND",
+    date: "28 Jan, 2021 - 04 Jan, 2023",
+    text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+  },
+  {
+    id: 1,
+    img: "https://rb.gy/wzxzx",
+    city: "Toronto",
+    country: "CANADA",
+    date: "31 Jan, 2021 - 25 Jan, 2015",
+    text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+  },
+];
+
+export default Api;
