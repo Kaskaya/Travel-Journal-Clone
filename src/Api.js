bdefe6c8c89827c9267baac617f65ffc56374dd6
Kaskaya <recep.tkaskaya@gmail.com>
2023-08-28 06:19:50 +0300

    configuration

--- src/Api.js
+++ src/Api.js
@@ -9,7 +9,7 @@ const Api = [
     text: "Amsterdam, capital of the Netherlands! These days the city has a population of just over 790.000 inhabitants and is the largest city in the country. Amsterdam is located in the province ‘Noord-Holland’, situated in the west. It is one of the most popular destinations in Europe, receiving more than 4.5 million tourists annually Amsterdam has a great history. It is very unique for its large and untouched historic center. It has a rich architectural history, dominated by water. It is a meeting point for all different cultures around the world and has a welcoming attitude towards visitors. Well known for its museums, red light district, coffee shops but also the great variety of eating & drinking places and night life.",
   },
   {
-    id: 1,
+    id: 2,
     img: "https://rb.gy/wld93",
     city: "London",
     country: "ENGLAND",
@@ -18,7 +18,7 @@ const Api = [
     text: "London, city, capital of the United Kingdom. It is among the oldest of the world’s great cities—its history spanning nearly two millennia—and one of the most cosmopolitan. By far Britain’s largest metropolis, it is also the country’s economic, transportation, and cultural centre.London is situated in southeastern England, lying astride the River Thames some 50 miles (80 km) upstream from its estuary on the North Sea. In satellite photographs the metropolis can be seen to sit compactly in a Green Belt of open land, with its principal ring highway (the M25 motorway) threaded around it at a radius of about 20 miles (30 km) from the city centre. The growth of the built-up area was halted by strict town planning controls in the mid-1950s. ",
   },
   {
-    id: 1,
+    id: 3,
     img: "https://rb.gy/wzxzx",
     city: "Toronto",
     country: "CANADA",
